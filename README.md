e51ebf6256b3368169acbb907434d176b53f4006
Daniel-bin <daniel.coughlan@outlook.ie>
2020-11-01 20:53:38 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# thinkster-v2qb3z-t3kazq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/thinkster-v2qb3z-t3kazq)
---
+++ src/app/flop-suggestion/flop-suggestion.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  templateUrl: './flop-suggestion.component.html'
+  
+})
+export class FlopSuggestionComponent  {
+
+  suggestions = []
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+  addSuggestion(suggestion) {
+    this.suggestions.push(suggestion);
+  }
+
+}
